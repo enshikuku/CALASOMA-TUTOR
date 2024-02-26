ee0828295d0034890913f76653967f9877c1b6f7
Shikuku39 <enshikuku39@gmail.com>
2024-02-26 13:40:55 +0300

    nodemailer

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const nodemailer = require('nodemailer')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.json())
+
+app.use(session({
+    secret: 'c@!@$0m@',
+    saveUninitialized: false,
+    resave: true
+}))
+
+dotenv.config()
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -27,22 +43,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-dotenv.config()
-
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended: true }))
-
-app.use(express.json())
-
-app.use(session({
-    secret: 'c@!@$0m@',
-    saveUninitialized: false,
-    resave: true
-}))
-
 app.use((req, res, next) => {
     res.locals.isLogedIn = (req.session.userID !== undefined)
     next()
@@ -52,19 +52,63 @@ function loginRequired(req, res) {
     res.locals.isLogedIn || res.redirect('/login')
 }
 
-function generateid() {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    const randomCharacters = Array.from({ length: 10 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
-    const genID = randomCharacters.join('')
-    return genID
+const offensiveWords = ['FUCK', 'SHIT', 'ASSHOLE', 'BITCH', 'DICK', 'CUNT', 'COCK', 'PUSSY', 'SLUT', 'WHORE', 'FAGGOT', 'MOTHERFUCKER', 'NIGGER', 'RETARD', 'TWAT', 'WANKER', 'ASSWIPE', 'BASTARD', 'DAMN', 'GODDAMN', 'ARSE', 'BOLLOCKS', 'BULLSHIT', 'CRAP', 'JACKASS', 'JERK', 'PISS',  'PRICK', 'SCREW', 'SUCK', 'TITS', 'ASSCLOWN', 'DUMBASS', 'FUCKER', 'SHITHEAD', 'ASSFACE', 'ASSHAT', 'CUM', 'DICKHEAD', 'ASSBAG', 'DIPSHIT', 'FUCKFACE', 'MOTHERFUCKING', 'NIGGA',  'SHITHOLE', 'ASSNUGGET', 'BASTARD', 'BLOWJOB', 'COCKSUCKER', 'CUMSLUT', 'DICKBAG', 'DICKWEED', 'DOUCHEBAG', 'FUCKTARD', 'JACKOFF', 'JIZZ', 'MOTHERFUCK', 'NIGGERS', 'PRICKFACE', 'SHITBAG', 'SHITSTAIN', 'TITFUCK', 'WANKSTAIN']
+
+function generateId() {
+    const characters = 'ABCDEFGHKMNPQRSTUVWXYZ23456789'
+    let genId
+    do {
+        const randomCharacters = Array.from({ length: 5 }, () => characters.charAt(Math.floor(Math.random() * characters.length)))
+        genId = randomCharacters.join('')
+    } while (offensiveWords.some(word => genId.includes(word)))
+    
+    return genId
 }
 
+const config = {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+}
+
+const sendMail = (data) => {
+    const transporter = nodemailer.createTransport(config)
+    transporter.sendMail(data, (error, info) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log('Email sent: ' + info.response)
+        }
+    })
+}
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/teachers-dashboard', (req, res) => {
-    res.render('teachers-dashboard')
+app.get('/login', (req, res) => {
+    const user = { 
+        email: 'enshikuku@gmail.com', 
+        password: '' 
+    }
+    res.render('login', { user, error: false })
+})
+
+app.post('/login', async (req, res) => {
+    const { email, subject, text } = req.body
+    const mailData = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: subject || 'Default Subject',
+        text: text || 'Default Text'
+    }
+
+    sendMail(mailData)
+    res.redirect('/login')
 })
 
 app.get('/logout', (req, res) => {
@@ -78,7 +122,7 @@ app.get('*', (req, res) => {
     res.render('404')
 })
 
-const PORT = process.env.PORT || 3009
+const PORT = process.env.PORT || 3004
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
